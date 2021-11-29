d3b79d32992ec80a3b8081659fad5c9f76f68d0e
jesusrubio3 <jesusrubiomartin3@gmail.com>
2021-11-29 10:11:15 +0000

    ficheros

---
+++ wagtaildemo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-a&4ykv9ej4ioe%r8kjw!lx9l(c7kcq4yefb72z@ls4@=cnqt=7'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
